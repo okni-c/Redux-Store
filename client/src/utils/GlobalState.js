36bac02d801961b8bc67e36b94e6dfcb00bbc714
okni-c <oknistay@gmail.com>
2021-07-25 19:44:02 -0500

    added globalstate redux

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,23 +1,28 @@
-import React, { createContext, useContext } from "react";
-import { useProductReducer } from './reducers'
+// import React, { createContext, useContext } from "react";
+// import { useProductReducer } from './reducers'
 
-const StoreContext = createContext();
-const { Provider } = StoreContext;
+// const StoreContext = createContext();
+// const { Provider } = StoreContext;
 
-const StoreProvider = ({ value = [], ...props }) => {
-  const [state, dispatch] = useProductReducer({
-    products: [],
-    cart: [],
-    cartOpen: false,
-    categories: [],
-    currentCategory: '',
-  });
+// const StoreProvider = ({ value = [], ...props }) => {
+//   const [state, dispatch] = useProductReducer({
+//     products: [],
+//     cart: [],
+//     cartOpen: false,
+//     categories: [],
+//     currentCategory: '',
+//   });
 
-  return <Provider value={[state, dispatch]} {...props} />;
-};
+//   return <Provider value={[state, dispatch]} {...props} />;
+// };
 
-const useStoreContext = () => {
-  return useContext(StoreContext);
-};
+// const useStoreContext = () => {
+//   return useContext(StoreContext);
+// };
 
-export { StoreProvider, useStoreContext };
+// export { StoreProvider, useStoreContext };
+
+import { createStore } from 'redux';
+import { reducer, initialState } from './reducers';
+
+export const store = createStore(reducer, initialState);
